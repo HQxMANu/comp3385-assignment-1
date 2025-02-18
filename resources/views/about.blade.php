beca7c662ad05008a76aa828326fbb4d60ed3868
Emmanuel Delgado <emmanuelpedrazadelgado@gmail.com>
2025-02-17 23:34:48 -0400

    Updated Blade template and fixed image path issue

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app') 
+@section('content') 
+    <h2>Emmanuel Delgado</h2> 
+    <p>I am a computer science major with a passion for web development and cloud computing. I have experience in backend development and AI applications, and I aim to become a cloud developer. I enjoy gym workouts and am always learning new technologies like React and Next.js.</p> 
+    <img src="{{ url('/images/chill_guy.gif') }}" alt="chill guy" /> 
+@endsection 
